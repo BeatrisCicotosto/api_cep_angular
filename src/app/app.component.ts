7e5cb31a266e859453bc116d80214367b5be088d
Beatris Cicotosto <biacicotosto@gmail.com>
2025-05-12 22:44:50 -0300

    commit api

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,41 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { HttpClient } from '@angular/common/http';
+
+
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,CommonModule, FormsModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Minha página em Angular';
+  cep: string = '';
+  endereco: any = null;
+  mensagemErro: string = '';
+
+  constructor(private http: HttpClient) {}
+
+  consultarCEP() {
+    this.mensagemErro = '';
+    this.endereco = null;
+    const cepLimpo = this.cep.replace(/\D/g, '');
+
+    const url = `https://viacep.com.br/ws/${cepLimpo}/json/`;
+    this.http.get<any>(url).subscribe({
+      next: (dados) => {
+        if (dados.erro) {
+          this.mensagemErro = 'CEP inválido ou endereço não encontrado.';
+        } else {
+          this.endereco = dados;
+        }
+      },
+      error: () => {
+        this.mensagemErro = 'Erro ao buscar o CEP.';
+      }
+    });
+  }
 }
